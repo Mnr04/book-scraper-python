d72c4294d150566e52c918c679180e4b518564f8
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2025-10-31 18:09:57 +0100

    last correction

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     #Loop Through Categories to Get Book URLs
     print("Fetching book URLs by category...")
 
-    for url in tqdm(urls_category[:3]):
+    for url in tqdm(urls_category):
         all_books_urls= retrieve_book_url(url, html)
 
     print("Successfully fetched all book URLs by category.")
